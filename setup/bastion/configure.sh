2a2e3169350066d421ea7bc1f85b32e1512608ef
Jason Anderson <jwa2@clemson.edu>
2018-08-06 23:49:23 -0400

    working on scripting benchmark installation

--- setup/bastion/configure.sh
+++ setup/bastion/configure.sh
@@ -13,7 +13,3 @@ echo "/nfs 10.0.0.0/16(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -a
 sudo service rpcbind start
 sudo service nfs start
-
-
-# fetch benchmarks
-git clone https://github.com/Rakurai/mpi-pingpong.git /nfs/repos/benchmarks/pingpong
---
+++ setup/bastion/prepareBenchmarks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+baseDir=/nfs/repos/benchmarks
+
+rm -rf ${baseDir}
+
+# fetch benchmark suites
+
+# mpi pingpong
+benchDir=${baseDir}/pingpong
+git clone https://github.com/Rakurai/mpi-pingpong.git ${benchDir}
+(cd ${benchDir} && make)
+
+# intel MPI
+benchDir=${baseDir}/intel-mpi
+git clone https://github.com/intel/mpi-benchmarks.git ${benchDir}
+cp ${benchDir}/src/make_mpich ${benchDir}/src/make_ompi
+sed -i 's|MPI_HOME=|MPI_HOME=/usr/lib64/openmpi|' ${benchDir}/src/make_ompi
+(cd ${benchDir}/src && make -f make_ompi)
+
+# NPB

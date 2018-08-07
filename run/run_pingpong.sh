c681c5373bdeacfa2395e511c8d3b5a9bd2a5106
Jason Anderson <jwa2@clemson.edu>
2018-08-06 23:49:23 -0400

    working on scripting benchmark installation

---
+++ run/run_pingpong.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z $2 ]; then
+	echo "usage: $0 <outfile> <iterations>"
+	exit
+fi
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+outfile=$1
+iters=$2
+
+trim=1000
+
+exe="mpiexec.openmpi"
+hostfile="~/hostfile"
+#rankfile="/nfs/files/scripts/env/mpi_ranks_bycore"
+executable="/nfs/repos/benchmarks/pingpong"
+mpi_params="-np 2 --hostfile ${hostfile}" #--rankfile ${rankfile}"
+out_params="2>/dev/null | tail -n ${iters}"
+
+${exe} ${mpi_params} ${executable} $((iters + trim)) ${out_params} > ${outfile}

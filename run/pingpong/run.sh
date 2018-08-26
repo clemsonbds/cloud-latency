0d86cdf5df385be1328691bc303ca6c8855ebbef
Jason Anderson <jwa2@clemson.edu>
2018-08-25 21:34:20 -0400

    debugging

--- run/pingpong/run.sh
+++ run/pingpong/run.sh
@@ -82,4 +82,4 @@ fi
 outFile="${resultDir}/pingpong-${resultName}.raw"
 
 echo Running pingpong between ${src} and ${dst}.
-mpirun ${mpiParams} ${executable} -t -i ${iters} -s ${skip} -b ${msgBytes} -d ${seconds} 1> ${outFile}
+echo "mpirun ${mpiParams} ${executable} -t -i ${iters} -s ${skip} -b ${msgBytes} -d ${seconds} 1> ${outFile}"

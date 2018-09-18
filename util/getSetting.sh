f10e99fe63d6e00d53295cd36058a22457cecc84
Jason Anderson <jwa2@clemson.edu>
2018-09-18 03:55:20 -0400

    Made getSetting.sh able to retrieve platform-specific settings.

--- util/getSetting.sh
+++ util/getSetting.sh
@@ -4,10 +4,20 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 settingsFile=${DIR}/../assets/settings.ini
 
 if [ -z "$1" ]; then
-	echo "usage: $0 <key>"
+	echo "usage: $0 <key> [platform]"
 	exit
 fi
 
 key=$1
+platform=${2:-"aws"}
 
-grep ${key} ${settingsFile} | cut -d'=' -f2
+# find platform-specific key first
+pair=`grep -i ${platform}-${key} ${settingsFile}`
+
+# if not found, try the general key
+if [ -z "${pair}" ]; then
+	pair=`grep -i ${key} ${settingsFile}`
+fi
+
+# if we found more than one, return the first
+echo "${pair}" | head -n1 | cut -d'=' -f2

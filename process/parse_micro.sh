3c18ad9585e2713eed58bcd3a9fa05d725dc0d7f
Jason Anderson <jwa2@clemson.edu>
2018-09-20 15:46:00 -0400

    Fixed up parsing script to handle both GCP and AWS, also iperf sample matrix.

--- process/parse_micro.sh
+++ process/parse_micro.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-rawDir=~/data/cloud-latency/results/raw/micro
-outDir=~/data/cloud-latency/results/parsed/micro
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+resultDir=${1:-"."}
+rawDir=${resultDir}/micro/raw
+outDir=${resultDir}/micro/parsed
 
 mkdir -p ${outDir}
+
+# get configuration names from files
+configNames=`echo ${rawDir}/pingpong-*-cross-*.raw | tr " " "\n" | cut -d- -f2,3,4,5 | sort | uniq`
 
 # parse raw pingpong output to individual CSV files
 for rawfile in ${rawDir}/pingpong*.raw; do
@@ -11,19 +17,7 @@ for rawfile in ${rawDir}/pingpong*.raw; do
 	filename="${filename%.*}"
 
 	echo Parsing ${filename}.raw
-	./parse_pingpong.py -o ${outDir}/${filename}.csv ${rawfile}
-done
-
-# create a CSV of pingpong latencies, one sample per column
-for grouping in cluster spread multi-az; do
-	for virt in vm metal; do
-		for f in ${outDir}/pingpong-${grouping}-${virt}-cross-*.csv; do
-			cut -d, -f3 ${f} > ${f}.temp
-		done
-
-		paste -d, ${outDir}/*.temp | awk 'NR>1' > ${outDir}/pingpong-${grouping}-${virt}-samples.csv
-		rm ${outDir}/*.temp
-	done
+	${DIR}/parse_pingpong.py -o ${outDir}/${filename}.csv ${rawfile}
 done
 
 # parse raw iperf output to individual CSV files
@@ -32,5 +26,32 @@ for rawfile in ${rawDir}/iperf*.json; do
 	filename="${filename%.*}"
 
 	echo Parsing ${filename}.json
-	./parse_iperf.py -o ${outDir}/${filename}.csv ${rawfile}
+	${DIR}/parse_iperf.py -o ${outDir}/${filename}.csv ${rawfile}
+done
+
+# create a CSV of measurements, one sample per column
+for benchmark in pingpong iperf; do
+for configName in ${configNames}; do
+
+	# Take the 'latency' column of each CSV file, create a new CSV file with those columns.
+	# Just to make it complicated, use the timestamp of each file as the column header.
+	for f in ${outDir}/${benchmark}-${configName}-cross-*.csv; do
+		temp=${f}.temp
+
+		# The third column is the latency, take that and strip the header
+		cut -d, -f3 ${f} | awk 'NR>1' > ${temp}
+
+		# Extract the timestamp from the filename (format is ###.timestamp.csv)
+		ts=`echo $(basename -- "$f") | sed -e 's/[^.]*.\([^.]*\).csv/\1/g'`
+
+		# Insert the timestamp as the first line.  Because of BSD sed on MacOS vs GNU sed
+		# on *nix systems, there is no portable way to do this without an intermediate backup
+		# file.  Also weird newline insertion syntax because Mac.
+		sed -i.bak '1s/^/'"${ts}"'\'$'\n/' ${temp}
+		rm ${temp}.bak
+	done
+
+	paste -d, ${outDir}/*.temp > ${outDir}/${benchmark}-samples-${configName}.csv
+	rm ${outDir}/*.temp
+done
 done

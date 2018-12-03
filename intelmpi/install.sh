bbb6f98e87cf29eba9e5399c9f40f5f72fb05dd2
Jason Anderson <jwa2@clemson.edu>
2018-12-03 10:29:18 -0500

    Finished build scripts for Intel MPI benchmarks

--- intelmpi/install.sh
+++ intelmpi/install.sh
@@ -3,8 +3,5 @@
 benchDir=impi
 git clone https://github.com/intel/mpi-benchmarks.git ${benchDir}
 
-# fix up for not using intel compiler
-
-
-
-sed -i 's|MPI_HOME=|MPI_HOME=/usr/lib64/openmpi|' ${benchDir}/src/make_ompi
+# replace with custom Makefile for non-Intel compiler, super fragile
+cp Makefile.mpicc ${benchDir}/src_cpp
--- setup/bastion/prepareBenchmarks.sh
+++ setup/bastion/prepareBenchmarks.sh
@@ -13,11 +13,10 @@ sed -i 's|/opt/local/include|/opt/local/include -std=gnu99|' ${benchDir}/Makefil
 (cd ${benchDir} && make)
 
 # intel MPI
-benchDir=${baseDir}/intel-mpi
-git clone https://github.com/intel/mpi-benchmarks.git ${benchDir}
-cp ${benchDir}/src/make_mpich ${benchDir}/src/make_ompi
-sed -i 's|MPI_HOME=|MPI_HOME=/usr/lib64/openmpi|' ${benchDir}/src/make_ompi
-(cd ${benchDir}/src && make -f make_ompi)
+benchDir="/nfs/repos/project/intelmpi"
+cd ${benchDir}
+./install.sh
+./build.sh
 
 # LAMMPS
 benchDir="/nfs/repos/project/lammps"

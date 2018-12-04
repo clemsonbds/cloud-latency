4e59d6fd88f61a7515c53851cc998a8d530b53fc
EC2 Default User <ec2-user@ip-10-0-1-62.ec2.internal>
2018-12-04 16:43:11 +0000

    modified test script and testing larger problem sizes

--- npb/run.sh
+++ npb/run.sh
@@ -10,9 +10,10 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 outpath=$1
 iters=$2
 
-hostfile="/nfs/files/scripts/env/mpi_hosts"
-rankfile="/nfs/files/scripts/env/mpi_ranks_bynode" # fill each node in order, change rankfile to distribute
-mpi_params="--mca btl ^tcp --rankfile ${rankfile}"
+hostfile="/nfs/instances"
+#rankfile="/nfs/files/scripts/env/mpi_ranks_bynode" # fill each node in order, change rankfile to distribute
+#mpi_params="--mca btl ^tcp --rankfile ${rankfile}"
+mpi_params="--hostfile ${hostfile}"
 out_params="2>/dev/null"
 
 execs=`ls ${BIN_DIR}`
@@ -30,6 +31,6 @@ for exec in $execs; do
 
  while [ `grep "Time in seconds" ${outfile} | wc -l` -lt ${iters} ]; do
 # for iter in `seq 1 ${iters}`; do
-  timeout 60 mpiexec.openmpi --np ${procs} ${mpi_params} ${BIN_DIR}/${exec} ${out_params} >> ${outfile}
+  mpirun --np ${procs} ${mpi_params} ${BIN_DIR}/${exec} ${out_params} >> ${outfile}
  done
 done

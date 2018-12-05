95120618ad0d82e177dcbfd9a3b7428e21854cb8
EC2 Default User <ec2-user@ip-10-0-1-158.ec2.internal>
2018-12-05 00:15:53 +0000

    fixes for test program

--- npb/run.sh
+++ npb/run.sh
@@ -13,24 +13,25 @@ iters=$2
 hostfile="/nfs/instances"
 #rankfile="/nfs/files/scripts/env/mpi_ranks_bynode" # fill each node in order, change rankfile to distribute
 #mpi_params="--mca btl ^tcp --rankfile ${rankfile}"
-mpi_params="--hostfile ${hostfile}"
-out_params="2>/dev/null"
+mpi_params="--hostfile ${hostfile} --rank-by node --mca plm_rsh_no_tree_spawn 1"
+#out_params="2>/dev/null"
 
-execs=`ls ${BIN_DIR}`
+#execs=`ls ${BIN_DIR}`
 
-for exec in $execs; do
- test=`echo $exec|tr '.' ' '|awk '{print $1}'`
- size=`echo $exec|tr '.' ' '|awk '{print $2}'`
- procs=`echo $exec|tr '.' ' '|awk '{print $3}'`
+for exec in ${BIN_DIR}/*; do
+ exn=`basename $exec`
+ test=`echo $exn|tr '.' ' '|awk '{print $1}'`
+ size=`echo $exn|tr '.' ' '|awk '{print $2}'`
+ procs=`echo $exn|tr '.' ' '|awk '{print $3}'`
 
  echo $test $size $procs
 
- outfile=${outpath}.${exec}.raw
+ outfile=${outpath}/${exn}.raw
 # rm -f ${outfile}
  touch ${outfile} # avoid 'file not found'
 
  while [ `grep "Time in seconds" ${outfile} | wc -l` -lt ${iters} ]; do
 # for iter in `seq 1 ${iters}`; do
-  mpirun --np ${procs} ${mpi_params} ${BIN_DIR}/${exec} ${out_params} >> ${outfile}
+  mpirun --np ${procs} ${mpi_params} ${exec} ${out_params} >> ${outfile}
  done
 done

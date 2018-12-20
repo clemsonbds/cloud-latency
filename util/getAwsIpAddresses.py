603391f317e1823041e6bd045ea783f665270c6e
EC2 Default User <ec2-user@ip-172-30-2-85.ec2.internal>
2018-12-20 19:23:14 +0000

    changed getting instance IPs to the new script

--- util/getAwsIpAddresses.py
+++ util/getAwsIpAddresses.py
@@ -9,16 +9,28 @@ import traceback
 
 region = sys.argv[1]
 expName = sys.argv[2]
+nodeType = sys.argv[3]
 
 try:
-    # aws ec2 --region ${region} describe-instances --filters "Name=tag:Name,Values=${expName}-BastionHost" "Name=instance-state-name,Values=running"
-    response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName) + '-BastionHost ', 'Name=instance-state-name,Values=running'], stderr=subprocess.STDOUT)
-    temp = json.loads(response)
+    if nodeType == "bastion":
+        # aws ec2 --region ${region} describe-instances --filters "Name=tag:Name,Values=${expName}-BastionHost" "Name=instance-state-name,Values=running"
+        response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName)+'-BastionHost ', 'Name=instance-state-name,Values=running'], stderr=subprocess.STDOUT)
+        temp = json.loads(response)
 
-    # Get the IP address from the instance list
-    print temp['Reservations'][0]['Instances'][0]['PublicIpAddress']
+        # Get the IP address from the instance list
+        print temp['Reservations'][0]['Instances'][0]['PublicIpAddress']
+
+    elif nodeType == "internal":
+        response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName)+'-Instance ', 'Name=instance-state-name,Values=running'], stderr=subprocess.STDOUT)
+        temp = json.loads(response)
+
+        for instance in temp['Reservations'][0]['Instances']:
+            print instance['PrivateIpAddress']
+
+    else:
+        usage()
+
 except subprocess.CalledProcessError as e:
     print ''.join(traceback.format_exc())
     print e.output
     sys.exit(0)
-
--- util/getInstanceIPs.sh
+++ util/getInstanceIPs.sh
@@ -9,7 +9,8 @@ expName=`${DIR}/getSetting.sh expName ${platform} | awk '{print tolower($0)}'`
 projectID=`${DIR}/getSetting.sh projectID ${platform}`
 
 if [ "${platform}" == "aws" ]; then
-	aws ec2 --region ${region} describe-instances --filters "Name=tag:Name,Values=${expName}-Instance" "Name=instance-state-name,Values=running" | grep PRIVATEIPADDRESSES | awk '{$1=$1};1' | cut -d' ' -s -f3 | sort -u
+	#aws ec2 --region ${region} describe-instances --filters "Name=tag:Name,Values=${expName}-Instance" "Name=instance-state-name,Values=running" | grep PRIVATEIPADDRESSES | awk '{$1=$1};1' | cut -d' ' -s -f3 | sort -u
+	python ${DIR}/getAwsIpAddresses.py ${region} ${expName} "internal"
 elif [ "${platform}" == "gcp" ]; then
 	python ${DIR}/getGcpIpAddresses.py ${projectID} ${expName} "internal"
 fi

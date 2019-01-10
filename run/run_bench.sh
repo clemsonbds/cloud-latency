306d1747965fdbbe9bba48217b0aa60e3e695641
Jason Anderson <jwa2@clemson.edu>
2019-01-10 11:45:49 -0500

    forgot a file

--- run/run_bench.sh
+++ run/run_bench.sh
@@ -5,31 +5,86 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 setupDir=${DIR}/../setup
 utilDir=${DIR}/../util
 bastionUtilDir="/nfs/repos/project/util"
-
-platform=${1:-"aws"}
-shift
-
-# extra arguments will be passed to the call to run_bench.sh on bastion
 nodeClassifier="/nfs/resources/getCpuIdentity.sh"
 hostfile="/nfs/mpi.hosts"
+numItersPerSet=1
+numItersPerProvision=1
+
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+aws)
+    platform="aws"
+    shift # past argument
+    ;;
+gcp)
+	platform="gcp"
+	shift
+	;;
+--platform)
+	platform="$2"
+	shift
+	shift
+	;;
+--hostfile)
+	hostfile="$2"
+	shift
+	shift
+	;;
+--numSets)
+	numItersPerSet="$2"
+	shift
+	shift
+	;;
+--numIters)
+	numItersPerProvision="$2"
+	shift
+	shift
+	;;
+--groupTypes)
+	groupTypes=`echo "$2" | tr ',' ' '`
+	shift
+	shift
+	;;
+--instanceTypes)
+	groupTypes=`echo "$2" | tr ',' ' '`
+	shift
+	shift
+	;;
+--skip_warmup)
+	skip_warmup=1
+	shift
+	;;
+--skip_micro)
+	skip_micro=1
+	shift
+	;;
+--skip_bench)
+	skip_bench=1
+	shift
+	;;
+*)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# extra arguments will be passed to the calls to run_bench.sh and run_micro.sh on bastion
 
 case ${platform} in
 aws)
-	numItersPerSet=5
-	numItersPerProvision=1
-	groupTypes+=" cluster"
-#	groupTypes+=" spread"
-#	groupTypes+=" multi-az"
-	instanceTypes+=" vm"
-#	instanceTypes+=" vmc5"
-#	instanceTypes+=" metal"
+	[ -z "${groupTypes}" ] && groupTypes="cluster spread multi-az"
+	[ -z "${instanceTypes}" ] && instanceTypes="vm vmc5 metal"
 	;;
 gcp)
-	numItersPerSet=6
-	numItersPerProvision=5
-	groupTypes+=" single-az"
-	groupTypes+=" multi-az"
-	instanceTypes+=" vm"
+	[ -z "${groupTypes}" ] && groupTypes="single-az multi-az"
+	[ -z "${instanceTypes}" ] && instanceTypes="vm"
 	groupClassifier="${bastionUtilDir}/bandwidthGroupClassifier.sh"
 	groupClasses="16Gb,10Gb" # in order, most desirable class first
 	groupClassOrder="descending" # match class order above
@@ -82,17 +137,23 @@ for instanceType in ${instanceTypes}; do
 
 		echo -e "\nRunning benchmarks for experiment configuration '${expType}'.\n"
 
-		echo Running warmup.
-		${utilDir}/sshBastion.sh ${platform} "~/project/run/bastion/run_bench.sh --expType warmup ${runParams} $@"
+		if [ -z "${skip_warmup}" ]; then
+			echo Running warmup.
+			${utilDir}/sshBastion.sh ${platform} "~/project/run/bastion/run_bench.sh --expType warmup ${runParams} $@"
+		fi
 
 		for j in `seq 1 ${numItersPerProvision}`; do
 			echo Starting iteration ${i}-${j}.
 
-			echo Running micro measurements.
-			${utilDir}/sshBastion.sh ${platform} "~/project/run/bastion/run_micro.sh --expType ${expType} ${runParams} $@"
+			if [ -z "{skip_micro}" ]; do
+				echo Running micro measurements.
+				${utilDir}/sshBastion.sh ${platform} "~/project/run/bastion/run_micro.sh --expType ${expType} ${runParams} $@"
+			fi
 
-			echo Running benchmarks.
-			${utilDir}/sshBastion.sh ${platform} "~/project/run/bastion/run_bench.sh --expType ${expType} ${runParams} $@"
+			if [ -z "{skip_bench}" ]; do
+				echo Running benchmarks.
+				${utilDir}/sshBastion.sh ${platform} "~/project/run/bastion/run_bench.sh --expType ${expType} ${runParams} $@"
+			fi
 		done
 
 		completed=1

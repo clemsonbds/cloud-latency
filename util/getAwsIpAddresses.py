4465dc20545341aa3a1d39f6015799e5c4d84981
Jason Anderson <jwa2@clemson.edu>
2019-01-10 10:13:49 -0500

    forced JSON output of aws cli responses

--- util/getAwsIpAddresses.py
+++ util/getAwsIpAddresses.py
@@ -14,14 +14,14 @@ nodeType = sys.argv[3]
 try:
     if nodeType == "bastion":
         # aws ec2 --region ${region} describe-instances --filters "Name=tag:Name,Values=${expName}-BastionHost" "Name=instance-state-name,Values=running"
-        response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName)+'-BastionHost ', 'Name=instance-state-name,Values=running'], stderr=subprocess.STDOUT)
+        response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName)+'-BastionHost ', 'Name=instance-state-name,Values=running', "--output", "json"], stderr=subprocess.STDOUT)
         temp = json.loads(response)
 
         # Get the IP address from the instance list
         print temp['Reservations'][0]['Instances'][0]['PublicIpAddress']
 
     elif nodeType == "internal":
-        response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName)+'-Instance ', 'Name=instance-state-name,Values=running'], stderr=subprocess.STDOUT)
+        response = subprocess.check_output(['aws', 'ec2', '--region', str(region), 'describe-instances', '--filters', 'Name=tag:Name,Values=' + str(expName)+'-Instance ', 'Name=instance-state-name,Values=running', "--output", "json"], stderr=subprocess.STDOUT)
         temp = json.loads(response)
 
         for instance in temp['Reservations'][0]['Instances']:
